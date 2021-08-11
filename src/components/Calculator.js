03ca4abdb4619913fa4f8e175169458aa16da270
Rhyine <rhyinestewart@gmail.com>
2021-08-11 17:23:55 -0500

    Change divs to buttons for calculator

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -13,25 +13,25 @@ class Calculator extends React.Component {
         <div className="Container">
           <div className="Display">0</div>
           <div className="Button-Container">
-            <div className="Button Normal">AC</div>
-            <div className="Button Normal">+/-</div>
-            <div className="Button Normal">%</div>
-            <div className="Button End">÷</div>
-            <div className="Button Normal">7</div>
-            <div className="Button Normal">8</div>
-            <div className="Button Normal">9</div>
-            <div className="Button End">x</div>
-            <div className="Button Normal">4</div>
-            <div className="Button Normal">5</div>
-            <div className="Button Normal">6</div>
-            <div className="Button End">-</div>
-            <div className="Button Normal">1</div>
-            <div className="Button Normal">2</div>
-            <div className="Button Normal">3</div>
-            <div className="Button End">+</div>
-            <div className="Button Normal Zero">0</div>
-            <div className="Button Normal">.</div>
-            <div className="Button End">=</div>
+            <button type="button" className="Button Normal">AC</button>
+            <button type="button" className="Button Normal">+/-</button>
+            <button type="button" className="Button Normal">%</button>
+            <button type="button" className="Button End">÷</button>
+            <button type="button" className="Button Normal">7</button>
+            <button type="button" className="Button Normal">8</button>
+            <button type="button" className="Button Normal">9</button>
+            <button type="button" className="Button End">x</button>
+            <button type="button" className="Button Normal">4</button>
+            <button type="button" className="Button Normal">5</button>
+            <button type="button" className="Button Normal">6</button>
+            <button type="button" className="Button End">-</button>
+            <button type="button" className="Button Normal">1</button>
+            <button type="button" className="Button Normal">2</button>
+            <button type="button" className="Button Normal">3</button>
+            <button type="button" className="Button End">+</button>
+            <button type="button" className="Button Normal Zero">0</button>
+            <button type="button" className="Button Normal">.</button>
+            <button type="button" className="Button End">=</button>
           </div>
         </div>
       </div>
