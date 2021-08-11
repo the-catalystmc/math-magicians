618ddd90cf492353f310765799527e55bc070cbc
Rhyine <rhyinestewart@gmail.com>
2021-08-11 18:32:13 -0500

    Update logic files and fix linter errors

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,26 +1,38 @@
 import '../scss/style.scss';
 import React from 'react';
+// import calculate from '../logic/calculate';
+// import operate from '../logic/operate';
 
 class Calculator extends React.Component {
   constructor(prop) {
     super(prop);
-    this.state = {};
+    this.state = {
+      total: 0,
+    };
+    this.updateTotal = this.updateTotal.bind(this);
+  }
+
+  updateTotal() {
+    this.setState(
+      { total: 5 },
+    );
   }
 
   render() {
+    const { total } = this.state;
     return (
       <div className="Calculator">
         <div className="Container">
-          <div className="Display">0</div>
+          <div className="Display">{ total }</div>
           <div className="Button-Container">
-            <button name="AC" type="button" className="Button Normal">AC</button>
+            <button onClick={this.updateTotal} name="AC" type="button" className="Button Normal">AC</button>
             <button name="+/-" type="button" className="Button Normal">+/-</button>
             <button name="%" type="button" className="Button Normal">%</button>
-            <button name="/" type="button" className="Button End">÷</button>
+            <button name="÷" type="button" className="Button End">÷</button>
             <button name="7" type="button" className="Button Normal">7</button>
             <button name="8" type="button" className="Button Normal">8</button>
             <button name="9" type="button" className="Button Normal">9</button>
-            <button name="*" type="button" className="Button End">x</button>
+            <button name="x" type="button" className="Button End">x</button>
             <button name="4" type="button" className="Button Normal">4</button>
             <button name="5" type="button" className="Button Normal">5</button>
             <button name="6" type="button" className="Button Normal">6</button>
