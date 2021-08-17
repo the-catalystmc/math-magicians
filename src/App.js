56c86ef5a6ffc06058b67c4c50481ca870acb8b9
Rhyine <rhyinestewart@gmail.com>
2021-08-17 14:46:19 -0500

    Style calculator and add new header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
+import Home from './components/Home';
 import Calculator from './components/Calculator';
 
 class App extends React.Component {
@@ -12,6 +13,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <Header />
+        <Home />
         <Calculator />
       </div>
     );
---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import '../scss/style.scss';
+import React from 'react';
+
+const Home = () => (
+  <div className="Home-Container">
+    <h2>Welcome to our page!</h2>
+    <p className="Home-Info">
+      Math magicians is a website for all fans of mathematics.
+      It is a Single Page App (SPA) that allows users to: Make simple calculations.
+      Read a random math-related quote.
+    </p>
+  </div>
+);
+
+export default Home;
