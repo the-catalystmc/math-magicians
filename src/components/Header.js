c3558a04761993e46a9403fe745e8d69537bccf8
Rhyine <rhyinestewart@gmail.com>
2021-08-17 13:52:39 -0500

    Style Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,12 @@ import React from 'react';
 const Header = () => (
   <div className="Header-Container">
     <h1>Math Magicians</h1>
-    <ul>
-      <li><a href="fpl.com">Home</a></li>
-      <li><a href="fpl.com">Calculator</a></li>
-      <li><a href="fpl.com">Qoute</a></li>
+    <ul className="Nav-Items">
+      <li className="Nav-Item"><a href="fpl.com">Home</a></li>
+      <span className="Nav-Item">|</span>
+      <li className="Nav-Item"><a href="fpl.com">Calculator</a></li>
+      <span className="Nav-Item">|</span>
+      <li className="Nav-Item"><a href="fpl.com">Qoute</a></li>
     </ul>
   </div>
 );
