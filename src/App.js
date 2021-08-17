6bf2be2ae23d3e1020e0ce7765b6c57f9aa916d8
Rhyine <rhyinestewart@gmail.com>
2021-08-17 17:50:17 -0500

    Apply Router to each component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import Header from './components/Header';
+import {
+  Route, BrowserRouter as Router, Switch, Link,
+} from 'react-router-dom';
+// import Header from './components/Header';
 import Home from './components/Home';
 import Calculator from './components/Calculator';
 import Quote from './components/Quote';
@@ -12,12 +15,25 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="App">
-        <Header />
-        <Home />
-        <Calculator />
-        <Quote />
-      </div>
+      <Router>
+        <Switch>
+          <div className="App">
+            <div className="Header-Container">
+              <h1>Math Magicians</h1>
+              <ul className="Nav-Items">
+                <li className="Nav-Item"><Link to="/">Home</Link></li>
+                <span className="Nav-Item">|</span>
+                <li className="Nav-Item"><Link to="/calculator">Calculator</Link></li>
+                <span className="Nav-Item">|</span>
+                <li className="Nav-Item"><Link to="/quote">Quote</Link></li>
+              </ul>
+            </div>
+            <Route path="/" exact component={Home} />
+            <Route path="/calculator" component={Calculator} />
+            <Route path="/quote" component={Quote} />
+          </div>
+        </Switch>
+      </Router>
     );
   }
 }
